b5713b58cc8961382686648ef90689356191b4ca
Mariano Eduardo Moreno Ragni <mamoreno@ar0c02vv0p1hv2j.fibertel.com.ar>
2023-05-06 19:42:47 -0300

    parImpar

--- src/main/java/SegundaClase/ParesImpares.java
+++ src/main/java/SegundaClase/ParesImpares.java
@@ -1,6 +1,33 @@
 package SegundaClase;
 
+import java.util.Scanner;
+
 public class ParesImpares {
 
+    public static void main(String[] args) {
 
-}
+
+        Scanner sc = new Scanner(System.in);
+        int acum;
+        int contadorPar=0;
+        int contadorImpar=0;
+        int suma;
+        int num;
+
+    do {
+    System.out.println("Ingrese un numero entero");
+     num = sc.nextInt();
+     if (num%2==0){
+         contadorPar++;
+     }
+     if (num%2!=0){
+         contadorImpar++;
+     }
+        System.out.println("Cantidad de numeros pares "+ contadorPar );
+        System.out.println("Cantidad de numeros impares "+ contadorImpar );
+    }
+        while (num!=0);
+
+    }
+    }
+

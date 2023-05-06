02ae961c2d2f8fd80023317cf200e6ee3928b332
LucasMorenoArg <mamoreno@ar0c02vv0p1hv2j.fibertel.com.ar>
2023-05-06 20:31:14 -0300

    Cambios en acumuladores

--- src/main/java/SegundaClase/ParesImpares.java
+++ src/main/java/SegundaClase/ParesImpares.java
@@ -8,25 +8,27 @@ public class ParesImpares {
 
 
         Scanner sc = new Scanner(System.in);
-        int acum;
         int contadorPar=0;
         int contadorImpar=0;
-        int suma;
         int num;
 
+
     do {
-    System.out.println("Ingrese un numero entero");
+    System.out.println("Ingrese un numero entero o ingrese cero para salir");
      num = sc.nextInt();
-     if (num%2==0){
+     if (((num % 2) == 0) && (num != 0)){
          contadorPar++;
+
      }
-     if (num%2!=0){
+     if (((num % 2) != 0)){
          contadorImpar++;
+
      }
-        System.out.println("Cantidad de numeros pares "+ contadorPar );
-        System.out.println("Cantidad de numeros impares "+ contadorImpar );
+
     }
         while (num!=0);
+        System.out.println("Cantidad de numeros pares "+ contadorPar );
+        System.out.println("Cantidad de numeros impares "+ contadorImpar );
 
     }
     }

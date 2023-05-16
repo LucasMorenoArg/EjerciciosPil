36e828c9cef97db732499f27766a8ece2d5b7cfb
LucasMorenoArg <lucas.moreno.ragni@mi.unc.edu.ar>
2023-05-16 14:30:30 -0300

    Clase 6

---
+++ src/main/java/Modulo_01/Clase_06/Administrativo.java
@@ -0,0 +1,24 @@
+package Modulo_01.Clase_06;
+
+public class Administrativo extends Empleado {
+
+    private boolean presentismo;
+
+    public Administrativo(int legajo, String nombre, double sueldoBasico, boolean presentismo) {
+        super(legajo, nombre, sueldoBasico);
+        this.presentismo = presentismo;
+    }
+
+    public boolean isPresentismo() {
+        return presentismo;
+    }
+
+    public void setPresentismo(boolean presentismo) {
+        this.presentismo = presentismo;
+    }
+
+    @Override
+    public double calcularSueldo() {
+        return getSueldoBasico() * (presentismo ? 1.13 : 1);
+    }
+}
---
+++ src/main/java/Modulo_01/Clase_06/Obrero.java
@@ -0,0 +1,24 @@
+package Modulo_01.Clase_06;
+
+public class Obrero extends Empleado {
+
+    private int diasTrabajados;
+
+    public Obrero(int legajo, String nombre, double sueldoBasico, int diasTrabajados) {
+        super(legajo, nombre, sueldoBasico);
+        this.diasTrabajados = diasTrabajados;
+    }
+
+    public int getDiasTrabajados() {
+        return diasTrabajados;
+    }
+
+    public void setDiasTrabajados(int diasTrabajados) {
+        this.diasTrabajados = diasTrabajados;
+    }
+
+    @Override
+    public double calcularSueldo() {
+        return getSueldoBasico() / 20 * diasTrabajados;
+    }
+}
